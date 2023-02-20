78e0dd9fe45422ae1e4024242e195bbb5bbcf35c
C.K GIDEON <myemail@gmail.com>
2023-02-19 22:48:23 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with put function
+ * Retutn:0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
