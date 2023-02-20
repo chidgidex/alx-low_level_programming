7a589988d7cef9cd7a4405edc8a7aba96261f300
C.K GIDEON <myemail@gmail.com>
2023-02-20 03:10:47 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - prints and that piece of art is usefu,
  * followed by a new line, to stansard error
  * Return: Always 1.
  */
