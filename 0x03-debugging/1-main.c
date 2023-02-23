bbf64e0c80735cf0a586667f4ac0b9790b9eeda4
C.K GIDEON <myemail@gmail.com>
2023-02-23 08:04:26 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,8 +5,11 @@
 * Return: 0
 */
 
+
+
 int main(void)
 {
+
 int i;
 
 printf("Infinite loop incoming :(\n");
@@ -17,4 +20,5 @@ i = 0;
 /*}*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
+
 }
