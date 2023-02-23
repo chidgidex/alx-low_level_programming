50a0b866e14a013f4269bf3b2c84957a3afc05b5
C.K GIDEON <myemail@gmail.com>
2023-02-23 15:20:48 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
-
 /**
- * more_numbers - print 10 timesthe number since 0 up to 14
- * Return: 10 times of numbers since 0 up to 14
+ * more_numbers - print 10 times the numbers since 0 up to 14
+ * Return: 10 times of the numbers since 0 up to 14
  */
 
 void more_numbers(void)
-{
 
+{
 	int x, y;
 
 	for (x = 0; x < 10; x++)
