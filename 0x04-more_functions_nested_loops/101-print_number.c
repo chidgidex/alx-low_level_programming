d7bfc3c5228e739158cf2fc8a42a9d370ca42309
C.K GIDEON <myemail@gmail.com>
2023-02-23 18:04:48 -0800

    ./101-print_numbers update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_number - Prints an integer
- * @: The integer to prints
- * Return: Nothing!
+ * @n: The integer to be printed
  */
 
 void print_number(int n)
@@ -13,16 +11,15 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-	n1 = -n;
-	_putchar('-');
-	}
-	else
+		n1 = -n;
+		_putchar('-');
+	} else
 	{
-	n1 = n;
+		n1 = n;
 	}
 	if (n1 / 10)
 	{
-	print_number(n1 / 10);
+		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
 }
