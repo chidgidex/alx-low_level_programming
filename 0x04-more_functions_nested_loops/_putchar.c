46a50a009029fabb8c8dad9f2c9cf425348d34ca
C.K GIDEON <myemail@gmail.com>
2023-02-23 16:06:39 -0800

    ./5-more_numbers

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,20 +1,14 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 0.
+ * _putchar - Write the character c to the stdout
+ * @c: The character to print
+ *
+ * Return: On Success 1.
+ * On error: -1 is returned, and errno is set appropriately
  */
-
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
