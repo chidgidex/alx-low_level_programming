32aa6f065976fe05f9dfb6245f8c00140faadd6e
C.K GIDEON <myemail@gmail.com>
2023-03-09 00:46:12 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -13,11 +13,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	int longit = 0;
 
 	if (*s)
 	{
-		len*+ ;
-		len += _strlen_recursion(s + 1);
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
+	return (longit);
 }
