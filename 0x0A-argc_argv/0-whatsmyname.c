d38797adb55cff789604e40728096a558d3d06df
C.K GIDEON <myemail@gmail.com>
2023-03-15 20:09:58 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,10 @@
  *
  * Return: Always 0 (success)
  */
-int main(int argc _attribute_((unused)), char *argv[1])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	(void) argc;
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
